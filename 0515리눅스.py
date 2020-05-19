d14380b81a95c0debc7a6065bf0b42b45e376e9f
formroller <54848570+formroller@users.noreply.github.com>
2020-05-19 18:24:01 +0900

    Update 0515리눅스.py

--- 0515리눅스.py
+++ 0515리눅스.py
@@ -69,7 +69,7 @@ echo $username
 
 echo -e :\c 기호 해석
 
-if문
+#[if문] 2_read.sh
 #!/bin/sh
 
 echo "사용자명을 입력하세요 : \c"
@@ -83,7 +83,7 @@ esle
 fi
 
 
-[파일 찾기]
+[파일 찾기] 3_read.sh
 #!/bin/sh
 
 echo "파일명을 입력하시오 : \c"
@@ -99,7 +99,7 @@ fi
 :w (저장)
 
 
-#[연습문제]
+#[연습문제] 4_read.sh
 #사용자로부터 파일명을 입력받고
 #해당 파일의 라인수가 50라인 이상이면 큰 파일입니다.
 #50라인 이하면 작은 파일입니다. 출력
@@ -150,7 +150,7 @@ then
   echo "$filename은 작은 파일입니다."
  fi
 
-#[연습문제]
+#[연습문제] 6_find.sh
 #파일을 특정 디렉터리로부터 찾아 출력하는 쉘 프로그램 작성
 #사용자로부터 찾을 디렉터리명 입력(1 depth로만 찾기)
 #사용자로부터 찾을 파일의 패턴 입력(abc입력시 abc를 포함하는 파일 찾기)
@@ -181,7 +181,7 @@ done
 echo
 echo
 
-#[for]
+#[for] 
 1)
 for i in a b c d   *리눅스는 인자의 나열
 do
@@ -189,7 +189,7 @@ do
 done
 
 
-2)
+2) 7_for.sh
 j=1
 for i in a b c d
 do 
@@ -197,7 +197,7 @@ do
  j=`expr $j + 1`
 done
 
-3) 
+3) 8_for2.sh
 #!/bin/sh
 
 #현 디렉터리에서 *.sh파일 목록을 for문으로 출력
@@ -212,7 +212,7 @@ do
  echo $i
 done
 
-#[연습문제]
+#[연습문제] 9_for3.sh
 #현재 디렉터리의 .sh파일에 대해 다음과 같은 형식으로 출력
 
 #파일명 : a.sh  파일라인수 : 7
@@ -223,6 +223,7 @@ done
 for fname in $(ls *.sh)
 do 
  lcnt=`cat $fname | wc -l`
+#lcnt=$(cat $fname | wc -l)
  echo "파일이름 : $fname \t 파일라인수 : $lcnt"
 done
 
@@ -240,18 +241,18 @@ host - ip 주소
 # =============================================================================
 cut -d: -f6 /etc/passwd
 
-grep jun /etc/passwd | cut -d: -6
+grep jun /etc/passwd | cut -d: -f6
   cut -d: -f6
   -d 분리구분기호
   -f6 분리된 구분기호중 몇번째 필드 가져올 것인지
 
 
 ls -l *.sh
-ls -l. *sh | cut -d" " -f 
+ls -l. *sh | cut -d" " -f1
  - 공백일 경우 -d" "
  - 모든 라인의 공백 다를 경우 cut 출력시 조건에 맞지 않는 값은 공백출력
  
-#[연습문제]
+#[연습문제] 10_for4.sh
 # 현재 디렉토리에서 .sh파일에 대해 아래와 같이 출력 (test3)
 
 #파일명 : a.sh  파일라인수 : 7    파일크기 : 10
@@ -267,17 +268,42 @@ for fname in $(ls *.sh)
 done
  
  
-# [연습문제]
+# [연습문제] 11_ex.sh
  디렉터리와 파일 패턴을 입력받고
  파일사이즈가 100이상인 파일의 이름과 번호 함께 출력
  복사할 파일번호 사용자에게 입력 받은 뒤 
  해당 파일을 임의의 디렉터리에 복사하는 셀 작성
-    
+ 
 
 #파일명 : a.sh  파일라인수 : 7
 #파일명 : b.sh  파일라인수 : 10
 
 #!/bin/sh
+
+# 사용자에게 디렉터리, 파일명 입력받고 사이즈 100 이상인 파일 복사
+ 
+echo "파일 찾을 디렉터리 위치 :\c"
+read dname
+
+echo "찾을 패턴 :\c"
+read fname
+
+fnum=1
+for flist in $(find $dname -maxdepth 1 -name "*$fname*" -type f):
+do 
+ fsize=`ls -l $flist | cut -d" " -f5`
+ if [ $fsize -ge 100 ]:
+ then 
+   echo $fnum : $flist
+   fnum=`expr $fnum + 1`
+  fi
+done
+
+echo "복사할 파일번호 입력 :\c "
+read vnum
+
+
+
 
 for fname in $(ls *.sh)
 do 
